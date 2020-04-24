55948f78c6ff8f04c217d8af105564c3e4ab8cd9
Azlan <vhussein80@gmail.com>
2020-04-24 15:08:53 +0800

    Enhancements based on feedback

--- src/main/java/com/azlan/test/clientaccount/controller/AccountController.java
+++ src/main/java/com/azlan/test/clientaccount/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.azlan.test.clientaccount.controller;
 
 import com.azlan.test.clientaccount.model.Account;
+import com.azlan.test.clientaccount.model.ResponseModel;
 import com.azlan.test.clientaccount.model.entity.AccountEntity;
 import com.azlan.test.clientaccount.service.ifc.AccountServiceIfc;
 import io.swagger.annotations.Api;
@@ -9,9 +10,11 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -31,21 +34,21 @@ public class AccountController {
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public  Iterable<AccountEntity> getAllAccounts(){
         log.debug("Get all accounts");
         return accountServiceIfc.getAllAccounts();
     }
 
-    @GetMapping(path = "/{clientId}")
+    @GetMapping(path = "/{clientId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity <List<AccountEntity>> getAccountByClientId(@PathVariable("clientId") final Long clientId) {
         log.debug("Get account by client ID: " + clientId);
         List<AccountEntity> account = accountServiceIfc.getAccountByClientId(clientId);
         return ResponseEntity.ok().body(account);
     }
 
-    @PostMapping(path = "/addAccount")
-    public ResponseEntity<String> createAccount(@RequestBody Account account) {
+    @PostMapping(path = "/addAccount", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseModel> createAccount(@RequestBody Account account) {
         log.debug("Creating account");
         
         AccountEntity accountEntity = new AccountEntity();
@@ -53,11 +56,12 @@ public class AccountController {
         accountEntity.setClientId(account.getClientId());
         accountServiceIfc.addAccount(accountEntity);
 
-        return ResponseEntity.status(HttpStatus.OK).body("");
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseModel(new Date(), "OK", "OK")
+        );
     }
 
-    @GetMapping(value = "/healthCheck")
-    public ResponseEntity<String> healthCheck(){
-        return ResponseEntity.status(HttpStatus.OK).body("I'm alive");
+    @GetMapping(value = "/healthCheck", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseModel> healthCheck(){
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseModel(new Date(), "I'm alive", ""));
     }
 }
